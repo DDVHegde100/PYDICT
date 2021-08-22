3e858be0cffa0ce22ca9ba5745c14f06912a35c6
Dhruv Hegde <69629876+DDVHegde100@users.noreply.github.com>
2021-08-22 15:22:35 -0400

    Add files via upload

---
+++ main - 2021-08-22T152230.434.py
@@ -0,0 +1,39 @@
+thisdict={
+"brand":"Ford",
+"model":"Mustang",
+"year":"1964"
+}
+print(thisdict)
+
+print(thisdict["brand"])
+
+x=thisdict["model"]
+print(thisdict)
+
+thisdict.popitem()
+print(thisdict)
+
+del thisdict["model"]
+print(thisdict)
+
+a=5
+b=6
+a!=b
+
+c=800
+d=31415
+e=69420
+
+if d>c:
+		print("Haha Lol. 31415 is very funny, it is 5 digits of Pi without decimals.")
+if e>c
+		print("69420 is god of universe")
+if c>e
+		print("I am an utter god, I broke Physics.")
+
+#There is for and while loops. 
+
+i=1
+while i<6:
+	print(i)
+  i+=1
